7045bdd874d460ce9d82636d28d36d3c30a5f2a8
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-26 15:44:59 +0300

    Problem solutions for Inheritance Lab

---
+++ Inheritance - Lab/CustomRandomList/CustomRandomList/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            int index = new Random().Next(0, Count);
+            string removedElement = this[index];
+            this.RemoveAt(index);
+            return removedElement;
+        }
+    }
+}
