f1e660387ef266983a3098462e617f22ae7485a5
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-27 17:41:59 +0300

    Problem solution added

---
+++ Inheritance - Exercise/Restaurant/Cake.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double DefaultGrams = 250;
+        private const double DefaultCalories = 1000;
+        private const decimal DefaultPrice = 5;
+        public Cake(string name)
+            : base(name, DefaultPrice, DefaultGrams, DefaultCalories)
+        {
+
+        }
+    }
+}
