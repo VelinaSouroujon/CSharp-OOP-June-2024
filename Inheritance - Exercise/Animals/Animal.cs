f206fbf35144ed46c6d52cc457b1543859ca5ffe
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-27 19:33:42 +0300

    Problem solution added

---
+++ Inheritance - Exercise/Animals/Animal.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private const string ExceptionMessage = "Invalid input!";
+
+        private string name;
+        private int age;
+        private string gender;
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name 
+        {
+            get
+            {
+                return name;
+            }
+            private set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessage);
+                }
+                name = value;
+            }
+        }
+        public int Age 
+        {
+            get
+            {
+                return age;
+            }
+            private set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessage);
+                }
+                age = value;
+            }
+        }
+        public string Gender 
+        {
+            get
+            {
+                return gender;
+            }
+            private set
+            {
+                string inputGender = value.ToLower();
+                if((inputGender != "male") && (inputGender != "female"))
+                {
+                    throw new ArgumentException(ExceptionMessage);
+                }
+
+                gender = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(GetType().Name);
+            sb.AppendLine($"{Name} {Age} {Gender}");
+            sb.AppendLine(ProduceSound());
+
+            return sb.ToString().Trim();
+        }
+        public abstract string ProduceSound();
+    }
+}
---
+++ Inheritance - Exercise/Animals/Engine.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Engine
+    {
+        private List<Animal> animals;
+
+        public Engine()
+        {
+            animals = new List<Animal>();
+        }
+        public void Run()
+        {
+            string type;
+            while ((type = Console.ReadLine()) != "Beast!")
+            {
+                try
+                {
+                    string[] animalArgs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                    string name = animalArgs[0];
+                    int age = int.Parse(animalArgs[1]);
+                    string gender = animalArgs[2];
+
+                    Animal animal = GetAnimal(type, name, age, gender);
+                    animals.Add(animal);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            PrintAnimals();
+        }
+        private Animal GetAnimal(string type, string name, int age, string gender)
+        {
+            Animal animal;
+
+            switch(type)
+            {
+                case "Dog":
+                    animal = new Dog(name, age, gender);
+                    break;
+
+                case "Cat":
+                    animal = new Cat(name, age, gender);
+                    break;
+
+                case "Frog":
+                    animal = new Frog(name, age, gender);
+                    break;
+
+                case "Kitten":
+                    animal = new Kitten(name, age);
+                    break;
+
+                case "Tomcat":
+                    animal = new Tomcat(name, age);
+                    break;
+
+                default:
+                    throw new ArgumentException();
+            }
+
+            return animal;
+        }
+        private void PrintAnimals()
+        {
+            foreach(Animal animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+    }
+}
