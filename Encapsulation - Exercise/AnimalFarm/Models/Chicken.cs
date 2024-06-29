ed8369f98d0826f902eac6800c59e2028be8c6c4
VelinaSouroujon <velinasouroujon@gmail.com>
2024-06-29 17:06:44 +0300

    Chicken.cs is now encapsulated

--- Encapsulation - Exercise/AnimalFarm/Models/Chicken.cs
+++ Encapsulation - Exercise/AnimalFarm/Models/Chicken.cs
@@ -1,17 +1,22 @@
-﻿namespace AnimalFarm.Models
+﻿using System;
+
+namespace AnimalFarm.Models
 {
     public class Chicken
     {
-        public const int MinAge = 0;
-        public const int MaxAge = 15;
+        private const int MinAge = 0;
+        private const int MaxAge = 15;
 
-        protected string name;
-        internal int age;
+        private const string InvalidNameMessage = "Name cannot be empty.";
+        private const string InvalidAgeMessage = "Age should be between {0} and {1}.";
+
+        private string name;
+        private int age;
 
         internal Chicken(string name, int age)
         {
-            this.name = name;
-            this.age = age;
+            Name = name;
+            Age = age;
         }
 
         public string Name
@@ -21,8 +26,13 @@
                 return this.name;
             }
 
-            internal set
+            private set
             {
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(InvalidNameMessage);
+                }
+
                 this.name = value;
             }
         }
@@ -34,8 +44,13 @@
                 return this.age;
             }
 
-            protected set
+            private set
             {
+                if(value < MinAge || value > MaxAge)
+                {
+                    throw new ArgumentException(string.Format(InvalidAgeMessage, MinAge, MaxAge));
+                }
+
                 this.age = value;
             }
         }
@@ -48,28 +63,22 @@
 			}
         }
 
-        public double CalculateProductPerDay()
+        private double CalculateProductPerDay()
         {
-            switch (this.Age)
+            if (Age <= 3)
             {
-                case 0:
-                case 1:
-                case 2:
-                case 3:
-                    return 1.5;
-                case 4:
-                case 5:
-                case 6:
-                case 7:
-                    return 2;
-                case 8:
-                case 9:
-                case 10:
-                case 11:
-                    return 1;
-                default:
-                    return 0.75;
+                return 1.5;
             }
+            if (Age <= 7)
+            {
+                return 2;
+            }
+            if(Age <= 11)
+            {
+                return 1;
+            }
+
+            return 0.75;
         }
     }
 }
--- Encapsulation - Exercise/AnimalFarm/Program.cs
+++ Encapsulation - Exercise/AnimalFarm/Program.cs
@@ -2,19 +2,26 @@
 {
     using System;
     using AnimalFarm.Models;
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            string name = Console.ReadLine();
-            int age = int.Parse(Console.ReadLine());
+            try
+            {
+                string name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
 
-            Chicken chicken = new Chicken(name, age);
-            Console.WriteLine(
-                "Chicken {0} (age {1}) can produce {2} eggs per day.",
-                chicken.Name,
-                chicken.Age,
-                chicken.ProductPerDay);
+                Chicken chicken = new Chicken(name, age);
+                Console.WriteLine(
+                    "Chicken {0} (age {1}) can produce {2} eggs per day.",
+                    chicken.Name,
+                    chicken.Age,
+                    chicken.ProductPerDay);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
