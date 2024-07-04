be06936bceae4bb91d4f7b3aa259f4e480329b10
VelinaSouroujon <velinasouroujon@gmail.com>
2024-07-04 22:11:19 +0300

    Problem solution added

---
+++ Interfaces and Abstraction - Exercise/ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitInterfaces
+{
+    public class Citizen : IResident, IPerson
+    {
+        public Citizen(string name, string country, int age)
+        {
+            Name = name;
+            Country = country;
+            Age = age;
+        }
+
+        public string Name { get; private set; }
+
+        public string Country { get; private set; }
+
+        public int Age { get; private set; }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {Name}";
+        }
+    }
+}
---
+++ Interfaces and Abstraction - Exercise/ExplicitInterfaces/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExplicitInterfaces
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            while((input = Console.ReadLine()).ToLower() != "end")
+            {
+                string[] citizenInfo = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = citizenInfo[0];
+                string country = citizenInfo[1];
+                int age = int.Parse(citizenInfo[2]);
+
+                Citizen citizen = new Citizen(name, country, age);
+                IResident resident = citizen;
+
+                Console.WriteLine(citizen.GetName());
+                Console.WriteLine(resident.GetName());
+
+            }
+        }
+    }
+}
