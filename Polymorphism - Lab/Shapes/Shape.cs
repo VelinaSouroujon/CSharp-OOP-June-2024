eff5b61ab49667aea3fb67e4fe9f30b763613594
VelinaSouroujon <velinasouroujon@gmail.com>
2024-07-07 17:51:43 +0300

    Problem solution added

---
+++ Polymorphism - Lab/Shapes/Shape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public abstract class Shape
+    {
+        public virtual string Draw()
+        {
+            return $"Drawing {GetType().Name}";
+        }
+        public abstract double CalculatePerimeter();
+        public abstract double CalculateArea();
+    }
+}
