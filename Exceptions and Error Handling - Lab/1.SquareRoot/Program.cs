167c2cc88d8e7e37c00f31af83f33ac2ea4acf0d
VelinaSouroujon <velinasouroujon@gmail.com>
2024-07-10 23:53:04 +0300

    Problem solution added

---
+++ Exceptions and Error Handling - Lab/1.SquareRoot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1.SquareRoot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            try
+            {
+                if(n < 0)
+                {
+                    throw new ArithmeticException("Invalid number.");
+                }
+
+                double result = Math.Sqrt(n);
+                Console.WriteLine(result);
+            }
+            catch(ArithmeticException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
