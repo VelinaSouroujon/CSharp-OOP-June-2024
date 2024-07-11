4bc1bf904b8ed3a49333e1be40edb5f6cb7d3feb
VelinaSouroujon <velinasouroujon@gmail.com>
2024-07-11 16:03:29 +0300

    Problem solution added

---
+++ Exceptions and Error Handling - Lab/4. SumOfIntegers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _4._SumOfIntegers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+
+            string[] input = Console.ReadLine()
+                .Split(' ');
+
+            foreach(string str in input)
+            {
+                try
+                {
+                    int currentNumber = int.Parse(str);
+                    sum += currentNumber;
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine($"The element '{str}' is in wrong format!");
+                }
+                catch(OverflowException)
+                {
+                    Console.WriteLine($"The element '{str}' is out of range!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{str}' processed - current sum: {sum}");
+                }
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+    }
+}
