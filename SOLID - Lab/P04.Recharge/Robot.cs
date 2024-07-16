f41be9c8e426edbdc93940a20f44e56eba86334f
VelinaSouroujon <velinasouroujon@gmail.com>
2024-07-16 21:00:59 +0300

    Remove methods that are not implemented

--- SOLID - Lab/P04.Recharge/Robot.cs
+++ SOLID - Lab/P04.Recharge/Robot.cs
@@ -23,7 +23,7 @@ namespace P04.Recharge
             set { this.currentPower = value; }
         }
 
-        public void Work(int hours)
+        public override void Work(int hours)
         {
             if (hours > this.currentPower)
             {
@@ -34,14 +34,9 @@ namespace P04.Recharge
             this.currentPower -= hours;
         }
 
-        public override void Recharge()
+        public void Recharge()
         {
             this.currentPower = this.capacity;
-        }
-
-        public override void Sleep()
-        {
-            throw new InvalidOperationException("Robots cannot sleep");
         }
     }
 }
