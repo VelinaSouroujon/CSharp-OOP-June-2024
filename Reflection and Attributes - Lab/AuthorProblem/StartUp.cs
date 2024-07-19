bb04859677f3c7112e651a4db8126e9a604ca798
VelinaSouroujon <velinasouroujon@gmail.com>
2024-07-19 23:49:44 +0300

    Add Tracker.cs that has method which prints the authors of the methods in StartUp.cs

--- Reflection and Attributes - Lab/AuthorProblem/StartUp.cs
+++ Reflection and Attributes - Lab/AuthorProblem/StartUp.cs
@@ -8,7 +8,8 @@ namespace AuthorProblem
         [Author("George")]
         static void Main(string[] args)
         {
-
+            Tracker tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
         }
     }
 }
---
+++ Reflection and Attributes - Lab/AuthorProblem/Tracker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+
+            MethodInfo[] methods = type.GetMethods(
+                BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+
+            foreach (MethodInfo method in methods)
+            {
+                AuthorAttribute author = method.GetCustomAttribute<AuthorAttribute>();
+                if (author is not null)
+                {
+                    Console.WriteLine($"{method.Name} is written by {author.Name}");
+                }
+            }
+        }
+    }
+}
