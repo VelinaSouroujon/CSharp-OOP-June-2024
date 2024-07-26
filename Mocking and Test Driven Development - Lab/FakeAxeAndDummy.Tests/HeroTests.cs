2171a9ba15d352ba6c74114e537e67295f2dd797
VelinaSouroujon <velinasouroujon@gmail.com>
2024-07-26 21:21:09 +0300

    Add Unit tests for Hero class using mocking

---
+++ Mocking and Test Driven Development - Lab/FakeAxeAndDummy.Tests/HeroTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+
+namespace FakeAxeAndDummy.Tests
+{
+    public class HeroTests
+    {
+        private Mock<IWeapon> mockWeapon;
+        private Hero hero;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mockWeapon = new Mock<IWeapon>();
+            hero = new Hero("MyHero", mockWeapon.Object);
+        }
+
+        [Test]
+        public void Attack_ValidData_GainExperience()
+        {
+            int targetExperience = 30;
+
+            Mock<ITarget> mockTarget = new Mock<ITarget>();
+
+            mockTarget.Setup(x => x.IsDead()).Returns(true);
+            mockTarget.Setup(x => x.GiveExperience()).Returns(targetExperience);
+
+            hero.Attack(mockTarget.Object);
+
+            Assert.AreEqual(targetExperience, hero.Experience);
+        }
+    }
+}
---
+++ Mocking and Test Driven Development - Lab/FakeAxeAndDummy/IWeapon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeAxeAndDummy
+{
+    public interface IWeapon
+    {
+        int AttackPoints { get; }
+        int DurabilityPoints { get; }
+        void Attack(ITarget target);
+    }
+}
