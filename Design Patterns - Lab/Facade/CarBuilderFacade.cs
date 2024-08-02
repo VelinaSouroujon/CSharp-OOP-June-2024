ec74fa470a6ea1c48e3c21242a91f7175837e1a2
VelinaSouroujon <velinasouroujon@gmail.com>
2024-08-02 22:01:28 +0300

    Problem solution added

---
+++ Design Patterns - Lab/Facade/CarBuilderFacade.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    public class CarBuilderFacade
+    {
+        public CarBuilderFacade()
+        {
+            Car = new Car();
+        }
+
+        protected Car Car { get; set; }
+
+        public CarInfoBuilder Info => new CarInfoBuilder(Car);
+        public CarAddressBuilder Built => new CarAddressBuilder(Car);
+
+        public Car Build() => Car;
+    }
+}
---
+++ Design Patterns - Lab/Facade/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Facade
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new CarBuilderFacade()
+                .Info
+                    .WithType("BMW")
+                    .WithColor("Red")
+                    .WithNumberOfDoors(4)
+                .Built
+                    .InCity("Munchen")
+                    .AtAddress("Random address")
+                .Build();
+
+            Console.WriteLine(car);
+        }
+    }
+}
